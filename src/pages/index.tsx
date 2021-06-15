55f6a10b506401f089655378a07cf0555316ad3a
Starchaser7 <starchaser73@live.com>
2021-06-15 11:13:11 -0300

    mexendo no index.style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,12 @@ import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironm
 import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
 import UserInformation from 'ui/components/data-display/UserInformation/UserInformation';
 import TextFieldMask from 'ui/components/inputs/TextFieldMask/TextFieldMask';
+import { Button, Typography } from '@material-ui/core';
+import {
+  FormElementsContainer,
+  ProfissionaisPaper,
+  ProfissionaisContainer,
+} from '@styles/pages/index.style';
 
 export default function Home() {
   return (
@@ -15,19 +21,65 @@ export default function Home() {
           'Preencha seu endereço e veja todos os profissionais da sua localidade'
         }
       />
-      <TextFieldMask
-        mask={'99.999-999'}
-        label={'Digite aqui seu CEP'}
-        fullWidth
-        variant={'outlined'}
-      />
 
-      <UserInformation
-        picture={'https://github.com/Starchaser7.png'}
-        name={'Eduardo'}
-        rating={4}
-        description={'Belo Horizonte'}
-      />
+      <FormElementsContainer>
+        <TextFieldMask
+          mask={'99.999-999'}
+          label={'Digite aqui seu CEP'}
+          fullWidth
+          variant={'outlined'}
+        />
+
+        <Typography color={'error'}>CEP inválido</Typography>
+        <Button
+          variant={'contained'}
+          color={'secondary'}
+          sx={{ width: '220px' }}
+        >
+          Buscar
+        </Button>
+      </FormElementsContainer>
+
+      <ProfissionaisPaper>
+        <ProfissionaisContainer>
+          <UserInformation
+            picture={'https://github.com/Starchaser7.png'}
+            name={'Eduardo'}
+            rating={4}
+            description={'Belo Horizonte'}
+          />
+          <UserInformation
+            picture={'https://github.com/Starchaser7.png'}
+            name={'Eduardo'}
+            rating={4}
+            description={'Belo Horizonte'}
+          />
+          <UserInformation
+            picture={'https://github.com/Starchaser7.png'}
+            name={'Eduardo'}
+            rating={4}
+            description={'Belo Horizonte'}
+          />
+          <UserInformation
+            picture={'https://github.com/Starchaser7.png'}
+            name={'Eduardo'}
+            rating={4}
+            description={'Belo Horizonte'}
+          />
+          <UserInformation
+            picture={'https://github.com/Starchaser7.png'}
+            name={'Eduardo'}
+            rating={4}
+            description={'Belo Horizonte'}
+          />
+          <UserInformation
+            picture={'https://github.com/Starchaser7.png'}
+            name={'Eduardo'}
+            rating={4}
+            description={'Belo Horizonte'}
+          />
+        </ProfissionaisContainer>
+      </ProfissionaisPaper>
     </div>
   );
 }
